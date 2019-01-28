5f5453e1fcf433a83717f7d64f02068862d8f2e2
bryanyuen <18yuent89@millshighsch.org>
2019-01-27 23:10:47 -0800

    New block

--- block.cpp
+++ block.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
-        cout << "Enter number of rows and columns: \n";
+        string prompt =  "Enter number of rows and columns: \n";
         int rows = 0;
         int col = 0;
+
+        do{
+        cout << prompt;
         cin >> rows >> col;
-        while (rows != 0 || col != 0){
-                for(int j = 1; j <= rows ; j++){
+             for(int j = 1; j <= rows ; j++){
                         cout <<"X." ;
-                for (int i = 1; i <= col - 1; i++){
-                cout << "X.";
+             for (int i = 1; i <= col - 1; i++){
+                        cout << "X.";
                 }
-                cout << "\n";
+                        cout << "\n";
         }
         if (rows == 0 || col == 0){
                 break;
         }
+        }while (rows != 0 || col != 0);
+
         return 0;
-}
 }
